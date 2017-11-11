2e444f7caf88df26bebce7c1be3467df89074a2f
Li Xuanji <xuanji@gmail.com>
2017-11-11 21:04:40 +0000

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Github Solidity Highlighter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.github.com/*.sol
+// @require      https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js
+// @require      https://cdn.jsdelivr.net/npm/highlightjs-solidity@1.0.5/solidity.min.js
+// @require      https://code.jquery.com/jquery-3.2.1.min.js
+// @resource     syntaxHighlightCSS https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // install highlightjs CSS
+    var syntax_highlighter_CSS = GM_getResourceText('syntaxHighlightCSS');
+    GM_addStyle(syntax_highlighter_CSS);
+
+    // install jQuery
+    const $ = jQuery;
+
+    // get code of page
+    const f = $(".file .blob-wrapper table .blob-code").map(function (i, row) {
+        return $(row).text().replace(/\n/g, "");
+    }).toArray();
+    const code = f.join("\n");
+
+    hljs.registerLanguage('solidity', hljsDefineSolidity);
+
+    $(".blob-wrapper").empty().append($("<code>", {
+        text: code,
+        class: "javascript",
+    }).wrap("<pre>").parent());
+
+    hljs.initHighlighting();
+
+})();
