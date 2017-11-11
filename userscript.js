cbc463683e0eb98a24fbf18d6033e64fc3c7ff5e
Li Xuanji <xuanji@gmail.com>
2017-11-11 21:53:41 +0000

    add .se files

--- userscript.js
+++ userscript.js
@@ -5,6 +5,7 @@
 // @description  synatx highlighting of ethereum languages (solidity, viper, LLL) on github
 // @author       ldct
 // @match        https://*.github.com/*.sol
+// @match        https://*.github.com/*.se
 // @require      https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js
 // @require      https://cdn.jsdelivr.net/npm/highlightjs-solidity@1.0.5/solidity.min.js
 // @require      https://code.jquery.com/jquery-3.2.1.min.js
@@ -16,12 +17,24 @@
 (function() {
     'use strict';
 
+    console.log("highlighting");
+
     // install highlightjs CSS
     var syntax_highlighter_CSS = GM_getResourceText('syntaxHighlightCSS');
     GM_addStyle(syntax_highlighter_CSS);
 
     // install jQuery
     const $ = jQuery;
+
+    // get language
+    var lang;
+    if (window.location.href.endsWith(".se")) {
+        lang = "python";
+    } else if (window.location.href.endsWith(".sol")) {
+        lang = "javascript";
+    } else {
+        return;
+    }
 
     // get code of page
     const f = $(".file .blob-wrapper table .blob-code").map(function (i, row) {
@@ -31,7 +44,7 @@
 
     $(".blob-wrapper").empty().append($("<code>", {
         text: code,
-        class: "javascript",
+        class: lang,
     }).wrap("<pre>").parent());
 
     hljs.initHighlighting();
